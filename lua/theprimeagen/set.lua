c55fee915a7bec14962ebc89bd9a17eb17f557a2
Rinsanga1 <rinsanga1221@gmail.com>
2024-10-06 19:50:02 +0530

    add everything

---
+++ lua/theprimeagen/set.lua
@@ -0,0 +1,79 @@
+-- Cursor appearance
+vim.opt.guicursor = ""
+
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indentation settings
+vim.opt.tabstop = 4      -- Number of visual spaces per tab
+vim.opt.softtabstop = 4  -- Spaces inserted when pressing <Tab>
+vim.opt.shiftwidth = 4   -- Spaces used for auto-indents
+vim.opt.expandtab = true -- Convert tabs to spaces
+
+-- Smart indentation
+vim.opt.smartindent = true
+
+-- Wrapping settings
+vim.opt.wrap = false
+
+-- File handling settings
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Search settings
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Terminal colors
+vim.opt.termguicolors = true
+
+-- Scrolling and display settings
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Update time for events
+vim.opt.updatetime = 50
+
+-- Color column for line length indication
+vim.opt.colorcolumn = "80"
+
+
+
+-- test transparency lua
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+
+-- transparent notify background
+vim.api.nvim_set_hl(0, "NotifyINFOBody", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyERRORBody", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyWARNBody", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyTRACEBody", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyINFOTitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyERRORTitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyWARNTitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyTRACETitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyINFOBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyERRORBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyWARNBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { bg = "none" })
